da20e19a418cc316911110f53ad1b1fe58792410
Maxime MAUSSION <maximemaussion30@gmail.com>
2024-05-16 19:14:32 +0200

    panic at the disco

---
+++ client/src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import error from "../assets/images/404.jpg";
+
+function NotFound() {
+  return (
+    <section className="NotFoundComponent">
+      <img src={error} alt="sylvain" />
+    </section>
+  );
+}
+
+export default NotFound;
