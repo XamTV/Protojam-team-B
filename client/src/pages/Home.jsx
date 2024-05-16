162733425206e314828a9a713bdaca4cdeed6ab4
Maxime MAUSSION <maximemaussion30@gmail.com>
2024-05-16 12:58:27 +0200

    HomePage done, need to import fonts

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,31 @@
+import { Link } from "react-router-dom";
+
 import "../style/Home.scss";
 
 export default function Home() {
   return (
-    <div id="HomeContainer">
-      <h1>Home</h1>
+    <div className="HomeContainer">
+      <h1>Le test de personnalité de Niavlys</h1>
 
-      <p>Home Page</p>
+      <p>
+        Entrez dans l'univers captivant de "PersonaliTREE", conçu pour vous
+        révéler la quintessence de votre être à travers le prisme de la nature.
+        En m'inspirant de la majesté et de la complexité des arbres, j'ai créé
+        ce projet avec sérieux, mais aussi avec une touche d'excentricité.
+        "PersonaliTREE" vous offre une exploration unique de votre essence
+        intérieure, en vous invitant à vous connecter à la sagesse et à la
+        beauté des arbres. À travers ce quiz, je vous accompagne dans un voyage
+        introspectif, où chaque réponse vous rapproche de la compréhension de
+        vous-même. Loin d'être seulement une fantaisie, "PersonaliTREE" est une
+        invitation à la réflexion profonde et à la découverte de soi.
+        Rejoignez-moi dans cette exploration fascinante où la nature et l'humain
+        se rencontrent, et découvrez ce que votre connexion avec le monde
+        végétal révèle sur votre être intérieur.{" "}
+      </p>
+
+      <button type="button">
+        <Link to="/quizz">Commencez le test</Link>
+      </button>
     </div>
   );
 }
