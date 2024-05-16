fa1a2f16492bef6d35fe5d25b5d02dfa58cb4d70
B4rka <p.e.dejonghe@gmail.com>
2024-05-16 17:24:57 +0200

    Creation de la methode results

--- server/src/Controller/QuizzController.php
+++ server/src/Controller/QuizzController.php
@@ -8,4 +8,16 @@ use App\Model\TreeManager;
 
 class QuizzController extends AbstractAPIController
 {
+    public function getResults(int $score)
+    {
+        $summaryManager = new SummaryManager();
+
+        if ($_SERVER['REQUEST_METHOD'] === "POST") { 
+            $score = $_POST['points'];
+            if ( $score >= 10 && $score <= 15) {
+                $result = $summaryManager->getSummary(1);
+            }
+            return json_encode($result);
+        }
+    }
 }
